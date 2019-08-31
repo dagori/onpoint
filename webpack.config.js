5fa7be5c40d4af57e539e5d18378caea412bc7e2
dagori <mariyav@protonmail.com>
2019-09-01 00:14:33 +0300

    public

--- webpack.config.js
+++ webpack.config.js
@@ -20,7 +20,7 @@ module.exports = {
   output: {
     filename: '[name].bundle.[hash].js',
     path: path.resolve(__dirname, 'dist'),
-    publicPath: '/'
+    publicPath: './src'
   },
   optimization: {
     splitChunks: {
